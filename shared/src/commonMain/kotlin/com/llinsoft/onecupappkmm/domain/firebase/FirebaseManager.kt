ac70e79f960d3176faf269682506c2456819626b
Haartag <90470991+Haartag@users.noreply.github.com>
2023-04-17 15:00:25 +0700

    Resource for FirebaseManager

--- shared/src/commonMain/kotlin/com/llinsoft/onecupappkmm/domain/firebase/FirebaseManager.kt
+++ shared/src/commonMain/kotlin/com/llinsoft/onecupappkmm/domain/firebase/FirebaseManager.kt
@@ -1,5 +1,6 @@
 package com.llinsoft.onecupappkmm.domain.firebase
 
+import com.llinsoft.onecupappkmm.Resource
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.auth.FirebaseAuth
 import dev.gitlive.firebase.auth.FirebaseUser
@@ -8,14 +9,17 @@ import dev.gitlive.firebase.auth.auth
 class FirebaseManager {
     private val auth: FirebaseAuth = Firebase.auth
 
-    suspend fun signUpWithEmail(email: String, password: String) {
-        try {
+    suspend fun signUpWithEmail(email: String, password: String): Resource<Unit> {
+        return try {
             val userCredential = auth.createUserWithEmailAndPassword(email, password)
             sendEmailVerification(userCredential.user)
+            Resource.Success(Unit)
         } catch (e: Exception) {
             // Handle sign up error
+            Resource.Error(e.message ?: "Unknown error")
         }
     }
+
 
     private suspend fun sendEmailVerification(user: FirebaseUser?) {
         user?.let {
