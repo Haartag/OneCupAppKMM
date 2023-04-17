7bf22a48c53203624da3d58003f4f98d606bd23f
Haartag <90470991+Haartag@users.noreply.github.com>
2023-04-17 13:25:04 +0700

    Android DI setup

---
+++ androidApp/src/main/java/com/llinsoft/onecupappkmm/android/CoffeeApp.kt
@@ -0,0 +1,8 @@
+package com.llinsoft.onecupappkmm.android
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoffeeApp: Application() {
+}
---
+++ androidApp/src/main/java/com/llinsoft/onecupappkmm/android/di/AppModule.kt
@@ -0,0 +1,19 @@
+package com.llinsoft.onecupappkmm.android.di
+
+import com.llinsoft.onecupappkmm.domain.firebase.FirebaseManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseManager(): FirebaseManager {
+        return FirebaseManager()
+    }
+}
