74dec5eca3b8eee71c761106912a5ebb8e2fe648
Haartag <90470991+Haartag@users.noreply.github.com>
2023-04-20 14:15:14 +0700

    Shared signIn fun to FirebaseManager

--- shared/src/commonMain/kotlin/com/llinsoft/onecupappkmm/domain/firebase/FirebaseManager.kt
+++ shared/src/commonMain/kotlin/com/llinsoft/onecupappkmm/domain/firebase/FirebaseManager.kt
@@ -9,6 +9,20 @@ import dev.gitlive.firebase.auth.auth
 class FirebaseManager {
     private val auth: FirebaseAuth = Firebase.auth
 
+    suspend fun signInWithEmail(email: String, password: String): Resource<Unit> {
+        return try {
+            val userCredential = auth.signInWithEmailAndPassword(email, password)
+            if (userCredential.user?.isEmailVerified == true) {
+                Resource.Success(Unit)
+            } else {
+                Resource.Error("Email not verified")
+            }
+        } catch (e: Exception) {
+            // Handle sign in error
+            Resource.Error(e.message ?: "Unknown error")
+        }
+    }
+
     suspend fun signUpWithEmail(email: String, password: String): Resource<Unit> {
         return try {
             val userCredential = auth.createUserWithEmailAndPassword(email, password)
@@ -19,6 +33,8 @@ class FirebaseManager {
             Resource.Error(e.message ?: "Unknown error")
         }
     }
+
+
 
 
     private suspend fun sendEmailVerification(user: FirebaseUser?) {
