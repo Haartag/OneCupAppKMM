9dd2c201cb11b803ba2e94d2feebd694158e5e33
Haartag <90470991+Haartag@users.noreply.github.com>
2023-04-23 18:52:59 +0700

    Shared Error handler update due to iOS compatibility.

--- shared/src/commonMain/kotlin/com/llinsoft/onecupappkmm/domain/firebase/ErrorHandler.kt
+++ shared/src/commonMain/kotlin/com/llinsoft/onecupappkmm/domain/firebase/ErrorHandler.kt
@@ -4,21 +4,23 @@ import com.llinsoft.onecupappkmm.util.ErrorTexts
 
 class ErrorHandler {
 
+    //error.contains due to difference in Android and iOS error messages
+
     fun getLoginErrorText(error: String): String {
-        return when(error) {
-            ErrorTexts.FirebaseLogin.VERIFY.errorText -> "Error: Please verify your email address."
-            ErrorTexts.FirebaseLogin.EMPTY.errorText -> "Error: Please enter your login and password."
-            ErrorTexts.FirebaseLogin.NOACC.errorText -> "Error: No such account exists."
-            ErrorTexts.FirebaseLogin.WPASS.errorText -> "Error: Incorrect password."
+        return when {
+            error.contains(ErrorTexts.FirebaseLogin.VERIFY.errorText) -> "Error: Please verify your email address."
+            error.contains(ErrorTexts.FirebaseLogin.EMPTY.errorText) -> "Error: Please enter your login and password."
+            error.contains(ErrorTexts.FirebaseLogin.NOACC.errorText) -> "Error: No such account exists."
+            error.contains(ErrorTexts.FirebaseLogin.WPASS.errorText) -> "Error: Incorrect password."
             else -> "An unknown error occurred"
         }
     }
 
     fun getRegistrationErrorText(error: String): String {
-        return when(error) {
-            ErrorTexts.FirebaseRegistration.SAME.errorText -> "Error: An account with this email " +
+        return when {
+            error.contains(ErrorTexts.FirebaseRegistration.SAME.errorText) -> "Error: An account with this email " +
                     "already exists. Please try another email or log in to your existing account."
-            ErrorTexts.FirebaseRegistration.NOEMAIL.errorText -> "Error: Invalid email format. " +
+            error.contains(ErrorTexts.FirebaseRegistration.NOEMAIL.errorText) -> "Error: Invalid email format. " +
                     "Please enter a valid email address. "
             else -> "An unknown error occurred"
         }
